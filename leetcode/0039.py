01d7ce751d8a206697f535b70ac1efad29d6afa2
winterdong666 <34205189+winterdong666@users.noreply.github.com>
2019-02-02 16:04:18 +0800

    .

---
+++ leetcode/0039.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        tmp = candidates[:]
+        res = [[]]
+        while len(tmp) > 0 and tmp[-1] > target:
+            tmp.pop()
+        if len(tmp) == 0:
+            if target > 0:
+                return []
+            else:
+                return res
+        res1 = self.combinationSum(tmp, target - tmp[-1])
+        for i in range(len(res1)):
+            res1[i].append(tmp[-1])
+        res2 = self.combinationSum(tmp[:-1], target)
+        return res1 + res2
