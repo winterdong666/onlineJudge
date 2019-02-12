de2fedec2bce56f58eb8cb7b2223799c0e4e6a5a
winterdong666 <34205189+winterdong666@users.noreply.github.com>
2019-02-12 16:15:30 +0800

    Same as 102

---
+++ leetcode/0107.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root: 'TreeNode') -> 'List[List[int]]':
+        if root == None:
+            return []
+        stack = [root]
+        layer = [0]
+        res = []
+        while len(stack) > 0:
+            node = stack.pop()
+            nodeLayer = layer.pop()
+            if nodeLayer >= len(res):
+                res.append([node.val])
+            else:
+                res[nodeLayer].append(node.val)
+            if node.right != None:
+                stack.append(node.right)
+                layer.append(nodeLayer + 1)
+            if node.left != None:
+                stack.append(node.left)
+                layer.append(nodeLayer + 1)
+        return res[::-1]
