69fa04aabcd4c161b7c8fd0326973f7419031056
winterdong666 <34205189+winterdong666@users.noreply.github.com>
2019-03-04 21:56:16 +0800

    SQL problem, pass

---
+++ leetcode/0196.py
@@ -0,0 +1,11 @@
+# This is an sql problem so I don't put solutions.
+
+'''
+# Write your MySQL query statement below
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email AND
+p1.Id > p2.Id
+'''
+
+pass
